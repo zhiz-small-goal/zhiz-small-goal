583b5b3759e076d0b2d03ef09b291db75610e080
zhiz <zhiz2023520@outlook.com>
2025-10-28 15:03:43 +0800

    详细编译步骤

---
+++ hello.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#define PI 3.14
+
+int main(){
+    std::cout << "hello, zhiz!" << std::endl;
+    std::cout << "PI = " << PI <<std::endl;
+    return 0;
+}
---
+++ hello.s
@@ -0,0 +1,68 @@
+	.file	"hello.cpp"
+	.text
+	.section .rdata,"dr"
+.LC0:
+	.ascii "hello, zhiz!\0"
+.LC1:
+	.ascii "PI = \0"
+	.text
+	.globl	main
+	.def	main;	.scl	2;	.type	32;	.endef
+	.seh_proc	main
+main:
+.LFB2190:
+	pushq	%rbp
+	.seh_pushreg	%rbp
+	movq	%rsp, %rbp
+	.seh_setframe	%rbp, 0
+	subq	$32, %rsp
+	.seh_stackalloc	32
+	.seh_endprologue
+	call	__main
+	leaq	.LC0(%rip), %rdx
+	movq	.refptr._ZSt4cout(%rip), %rax
+	movq	%rax, %rcx
+	call	_ZStlsISt11char_traitsIcEERSt13basic_ostreamIcT_ES5_PKc
+	movq	%rax, %rcx
+	movq	.refptr._ZSt4endlIcSt11char_traitsIcEERSt13basic_ostreamIT_T0_ES6_(%rip), %rax
+	movq	%rax, %rdx
+	call	_ZNSolsEPFRSoS_E
+	leaq	.LC1(%rip), %rdx
+	movq	.refptr._ZSt4cout(%rip), %rax
+	movq	%rax, %rcx
+	call	_ZStlsISt11char_traitsIcEERSt13basic_ostreamIcT_ES5_PKc
+	movsd	.LC2(%rip), %xmm0
+	movapd	%xmm0, %xmm1
+	movq	%rax, %rcx
+	call	_ZNSolsEd
+	movq	%rax, %rcx
+	movq	.refptr._ZSt4endlIcSt11char_traitsIcEERSt13basic_ostreamIT_T0_ES6_(%rip), %rax
+	movq	%rax, %rdx
+	call	_ZNSolsEPFRSoS_E
+	movl	$0, %eax
+	addq	$32, %rsp
+	popq	%rbp
+	ret
+	.seh_endproc
+	.section .rdata,"dr"
+	.align 8
+.LC2:
+	.long	1374389535
+	.long	1074339512
+	.def	__main;	.scl	2;	.type	32;	.endef
+	.ident	"GCC: (MinGW-W64 x86_64-ucrt-posix-seh, built by Brecht Sanders, r2) 15.2.0"
+	.def	_ZStlsISt11char_traitsIcEERSt13basic_ostreamIcT_ES5_PKc;	.scl	2;	.type	32;	.endef
+	.def	_ZNSolsEPFRSoS_E;	.scl	2;	.type	32;	.endef
+	.def	_ZNSolsEd;	.scl	2;	.type	32;	.endef
+	.section	.rdata$.refptr._ZSt4endlIcSt11char_traitsIcEERSt13basic_ostreamIT_T0_ES6_, "dr"
+	.p2align	3, 0
+	.globl	.refptr._ZSt4endlIcSt11char_traitsIcEERSt13basic_ostreamIT_T0_ES6_
+	.linkonce	discard
+.refptr._ZSt4endlIcSt11char_traitsIcEERSt13basic_ostreamIT_T0_ES6_:
+	.quad	_ZSt4endlIcSt11char_traitsIcEERSt13basic_ostreamIT_T0_ES6_
+	.section	.rdata$.refptr._ZSt4cout, "dr"
+	.p2align	3, 0
+	.globl	.refptr._ZSt4cout
+	.linkonce	discard
+.refptr._ZSt4cout:
+	.quad	_ZSt4cout
