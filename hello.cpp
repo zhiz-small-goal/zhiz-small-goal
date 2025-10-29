835925600af3eb2277648c6565f807d344f77603
zhiz <zhiz2023520@outlook.com>
2025-10-29 18:29:30 +0800

    测试不加std::endl的效果

--- hello.cpp
+++ hello.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 
 int main(){
-    std::cout << "hello, world, by zhiz!" << std::endl;
+    std::cout << "hello, world, by zhiz!";
+    std::cout << "1" << std::endl;
+    std::cout << "are you ok?";
     return 0;
 }
